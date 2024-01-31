f2c68823a60159a5e66aca6c2df92f6ec2955c90
sotojang <mrjang28@gmail.com>
2024-01-31 19:01:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,12 @@ from aqara_api import AqaraClient
 import traceback
 import json
 
-# 로그 파일을 저장할 디렉토리 경로를 설정합니다.
 log_dir = 'logs'
-
-# 로그 디렉토리가 존재하지 않으면 생성합니다.
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-# 현재 날짜를 기반으로 로그 파일명을 생성합니다.
 log_filename = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-
-# 로그 파일의 전체 경로를 생성합니다.
 log_file_path = os.path.join(log_dir, log_filename)
-
-# 로깅 설정을 구성합니다.
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s]: %(message)s',
